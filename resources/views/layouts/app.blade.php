0cbde39ac1279327b2e10e01b8a433445ad5f5d7
jesign <jixzignacio@gmail.com>
2017-01-18 16:31:38 +0800

    fix some bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,7 +49,7 @@
                     HeuriCode
                 </a>
                 <ul class="right hide-on-med-and-down">
-                    @if (Auth::guest())s
+                    @if (Auth::guest())
                         <li><a href="{{ url('/login') }}">Login</a></li>
                         <li><a href="{{ url('/register') }}">Register</a></li>
                     @endif
